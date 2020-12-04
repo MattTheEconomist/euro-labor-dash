39eaf8c90727db5d23b8da2b1d4642cda581b38f
Matt Ashford <mattashford1@gmail.com>
2020-12-04 15:37:48 -0500

    finished functions to generate fetch URLs

--- euro-labor-dash/src/components/ControlPanel.tsx
+++ euro-labor-dash/src/components/ControlPanel.tsx
@@ -1,43 +1,31 @@
-import React, {useState, useEffect} from 'react';
-import countryData from '../dataSources/countryCodes.json'
+import React from "react";
+import countryData from "../dataSources/countryCodes.json";
 
-interface ControlPanelProps{
-    // changeCountry():()=>void,
-    changeCountry:any,
-    propNumber: number,
-    anotherNumber: number
+interface ControlPanelProps {
+  // changeCountry():()=>void,
+  defaultCountry: string, 
+  changeCountry: any;
+//   anotherNumber: number;
 }
 
 
-// const ControlPanel: React.FC <Props> = (changeCountry) =>{
-const ControlPanel: React.FC <ControlPanelProps> = ({changeCountry, propNumber, anotherNumber}:ControlPanelProps) =>{
-// const ControlPanel: React.FC = () =>{
-    // const {changeCountry} = props
-    // console.log(propNumber.propNumber)
-    // console.log(propNumber, anotherNumber)
-    // const [selectedCountry, setSelectedCountry] = useState <string> ("Euro area")
-    
-    // function changeCountry(newCountry :string) :void{
-    //     setSelectedCountry(newCountry)
-    //     console.log(newCountry)
-    // }
 
-    // let countries  = countryData[0]
+export const ControlPanel: React.FC<ControlPanelProps> = ({
+    changeCountry,
+    defaultCountry
+  }) => {
+    let countryNames = Object.values(countryData[0]);
 
-    let countryNames = Object.values(countryData[0])
-    // console.log(countryNames)
+
+
+    return (<div>
+
+        <select onChange={(e) => changeCountry(e.target.value)} defaultValue={defaultCountry}>
+        {countryNames.map((country, ind)=> <option key={ind}>{country}</option> )}
+        </select>
+    </div>)  }
 
 
 
 
-return <div>
-<select onChange={e=>changeCountry(e.target.value)}>
-    {countryNames.map((country, ind) =><option key={ind} > {country}</option>)}
-    </select>
-
-    <h5>hi</h5>
- </div>
-
-}
-
-export default ControlPanel
+export default ControlPanel;
--- euro-labor-dash/src/components/DashContainer.tsx
+++ euro-labor-dash/src/components/DashContainer.tsx
@@ -1,24 +1,80 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import ControlPanel from "./ControlPanel";
-
+import data from "../dataSources/countryCodes.json";
 
 const DashContainer: React.FC = () => {
-    const [selectedCountry, setSelectedCountry] = useState <string> ("Euro area")
-    // const [selectedCountry, setSelectedCountry] = useState <string> ("Euro area")
+  const [selectedCountry, setSelectedCountry] = useState<string>("Euro area");
+  const [unemploymentURL, setUnemploymentURL] = useState<string>("")
+  const [vacanciesURL, setVacanciesURL] = useState<string>("")
+  const [netEarningsURL, setNetEarningsURL] = useState<string>("")
+  const [grossEarningsURL, setGrossEarningsURL] = useState<string>("")
 
-    function changeCountry(newCountry :string) :void{
-        setSelectedCountry(newCountry)
-        console.log(newCountry)
-    }
+
+  let countryData = data[0];
+
+  function changeCountry(newCountry: string): void {
+    setSelectedCountry(newCountry);
+  }
+
+  function getKeyByValue(object: any, value: string) {
+    return Object.keys(object).find((key) => object[key] === value);
+  }
+
+  function generateFetchURL_unemployemnt(selectedCountry: string) {
+    let countryCode = getKeyByValue(countryData, selectedCountry);
+
+    let fetchURL = `https://api.db.nomics.world/v22/series/Eurostat/une_rt_q/Q.SA.Y20-64.PC_ACT.T.${countryCode}?observations=True`;
+
+    return fetchURL;
+  }
+
+
+  
+  function generateFetchURL_vacancies(selectedCountry: string) {
+    let countryCode = getKeyByValue(countryData, selectedCountry);
+
+    let fetchURL = `https://api.db.nomics.world/v22/series/Eurostat/tps00172/Q.NSA.B-S.TOTAL.JOBRATE.${countryCode}?observations=True`;
+
+    return fetchURL;
+  }
+
     
+  function generateFetchURL_net_earnings(selectedCountry: string) {
+    let countryCode = getKeyByValue(countryData, selectedCountry);
 
-  return <div>
-      {/* <ControlPanel changeCountry={changeCountry}/> */}
-      <ControlPanel  changeCountry={changeCountry} propNumber={6} anotherNumber={7}/>
-      {/* <ControlPanel /> */}
+    let fetchURL = `https://api.db.nomics.world/v22/series/Eurostat/earn_nt_net/A.EUR.GRS.A1_80.${countryCode}?observations=True`;
+
+    return fetchURL;
+  }
 
 
-  </div>;
+      
+  function generateFetchURL_gross_earnings(selectedCountry: string) {
+    let countryCode = getKeyByValue(countryData, selectedCountry);
+
+    let fetchURL = `https://api.db.nomics.world/v22/series/Eurostat/earn_nt_net/A.PPS.GRS.A1_80.${countryCode}?observations=True`;
+
+    return fetchURL;
+  }
+
+
+
+
+
+  useEffect(() => {
+    setUnemploymentURL(generateFetchURL_unemployemnt(selectedCountry))
+    setVacanciesURL(generateFetchURL_vacancies(selectedCountry))
+    setNetEarningsURL( generateFetchURL_net_earnings(selectedCountry))
+    setGrossEarningsURL(generateFetchURL_gross_earnings(selectedCountry))
+  }, [selectedCountry]);
+
+  
+
+  return (
+    <div>
+      <ControlPanel changeCountry={changeCountry} defaultCountry="Euro area" />
+    </div>
+  );
 };
 
 export default DashContainer;
