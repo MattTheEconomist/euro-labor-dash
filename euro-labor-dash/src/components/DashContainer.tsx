f7440e0630f31984ecd9e2c35fc27eb8e0bdd4b9
Matt Ashford <mattashford1@gmail.com>
2020-12-08 10:23:33 -0500

    fetch working better but still not triggering properly

--- euro-labor-dash/src/components/DashContainer.tsx
+++ euro-labor-dash/src/components/DashContainer.tsx
@@ -1,161 +1,188 @@
 import React, { useState, useEffect } from "react";
 import ControlPanel from "./ControlPanel";
 import data from "../dataSources/countryCodes.json";
-import {Service} from '../types/Service'
-import {EconResponse}  from '../types/EconResponse'
-import  useFetchEconService  from '../services/useFetchEconService'
-
-
-
+import { Service } from "../types/Service";
+import { EconResponse } from "../types/EconResponse";
+import useFetchEconService from "../services/useFetchEconService";
+// import fetchEconService from "../services/useFetchEconService";
 
 const DashContainer: React.FC = () => {
-  const [selectedCountry, setSelectedCountry] = useState<string>("Euro area");
-  // const [unemploymentURL, setUnemploymentURL] = useState<string>("");
-  // const[unemployemnetSeries, setUnemploymentSeries] = useState ([])
-  // const [vacanciesURL, setVacanciesURL] = useState<string>("");
-  // const [netEarningsURL, setNetEarningsURL] = useState<string>("");
-  // const [grossEarningsURL, setGrossEarningsURL] = useState<string>("");
+  const [selectedCountry, setSelectedCountry] = useState<string>("");
+  const [unemploymentData, setUnemploymentData] = useState({});
 
-  // useEffect(()=>{
-  //   // export default selectedCountry
-  //   // export default selectedCountry
-  // }, [selectedCountry])
-  // export default selectedCountry
+  const service = useFetchEconService(selectedCountry);
 
-  // let countryData = data[0];
+    useEffect(() => {
+    setSelectedCountry("Euro area");
 
-  const service = useFetchEconService(selectedCountry)
+    if (service.status == "loaded") {
+      setUnemploymentData(returnLabelsAndValues(service))
+      }
+     
+      console.log(unemploymentData)
+
+
+  }, []);
+
+
 
   function changeCountry(newCountry: string): void {
     setSelectedCountry(newCountry);
   }
 
-  // function getKeyByValue(object: any, value: string) {
-  //   return Object.keys(object).find((key) => object[key] === value);
+  function returnLabelsAndValues(obj: any) :any|null {
+    interface rez {
+      labels?: any|undefined;
+      values?:any|undefined;
+      seriesName?: any|undefined;
+    } 
+
+    let rez = {
+      labels: null,
+      values: null,
+      seriesName: null,
+      
+    };
+
+    // if( String(obj.payload.series.docs[0].period)=='undefined'){return null}
+
+    rez["labels"] = obj.payload.series.docs[0].period;
+    rez["values"] = obj.payload.series.docs[0].value;
+    rez["seriesName"] = obj.payload.series.docs[0].series_name;
+
+    return rez;
+  }
+
+
+
+
+  
+  useEffect(()=>{
+
+    if (service.status == "loaded") {
+    setUnemploymentData(returnLabelsAndValues(service))
+    }
+   
+    console.log(unemploymentData)
+
+
+  },[selectedCountry])
+
+
+
+  // if (service.status == "loaded") {
+    // setUnemploymentData(returnLabelsAndValues(service));
+    // console.log(unemploymentData);
   // }
 
-  // function generateFetchURL_unemployemnt(selectedCountry: string) {
-  //   let countryCode = getKeyByValue(countryData, selectedCountry);
-
-  //   let fetchURL = `https://api.db.nomics.world/v22/series/Eurostat/une_rt_q/Q.SA.Y20-64.PC_ACT.T.${countryCode}?observations=True`;
-
-  //   return fetchURL;
-  // }
-
-  // function generateFetchURL_vacancies(selectedCountry: string) {
-  //   let countryCode = getKeyByValue(countryData, selectedCountry);
-
-  //   let fetchURL = `https://api.db.nomics.world/v22/series/Eurostat/tps00172/Q.NSA.B-S.TOTAL.JOBRATE.${countryCode}?observations=True`;
-
-  //   return fetchURL;
-  // }
-
-  // function generateFetchURL_net_earnings(selectedCountry: string) {
-  //   let countryCode = getKeyByValue(countryData, selectedCountry);
-
-  //   let fetchURL = `https://api.db.nomics.world/v22/series/Eurostat/earn_nt_net/A.EUR.GRS.A1_80.${countryCode}?observations=True`;
-
-  //   return fetchURL;
-  // }
-
-  // function generateFetchURL_gross_earnings(selectedCountry: string) {
-  //   let countryCode = getKeyByValue(countryData, selectedCountry);
-
-  //   let fetchURL = `https://api.db.nomics.world/v22/series/Eurostat/earn_nt_net/A.PPS.GRS.A1_80.${countryCode}?observations=True`;
-
-  //   return fetchURL;
-  // }
 
 
 
 
   // useEffect(() => {
-  //   setUnemploymentURL(generateFetchURL_unemployemnt(selectedCountry));
-    // setVacanciesURL(generateFetchURL_vacancies(selectedCountry));
-    // setNetEarningsURL(generateFetchURL_net_earnings(selectedCountry));
-    // setGrossEarningsURL(generateFetchURL_gross_earnings(selectedCountry));
-    // fetchUnemploymentData(unemploymentURL)
-  // }, [selectedCountry]);
+  //   const service = useFetchEconService(selectedCountry);
 
   
-  // useEffect(()=>{
- 
-  //   function fetchUnemploymentData(unemploymentURL : string){
-  //     fetch(unemploymentURL)
-  //     .then(rez=>rez.json())
-  //     // .then(data=>setUnemploymentSeries(data))
-  //     .then(data=>console.log(data))
-  //     // .then(()=> setUnemploymentSeries(unemployemnetSeries.dataset.series.docs[0].value))
-  //     // .then(()=>console.log(unemployemnetSeries))
-  //     .catch(err=>{console.log(err)})
-      
-  //       }
+  // }, [selectedCountry]);
 
-  //   fetchUnemploymentData(unemploymentURL)
-  // }, [unemploymentURL])
+  // useEffect(() => {
 
+  //   const service = fetchEconService(selectedCountry)
 
-
-// let dataOutput = service
-
-function returnLabelsAndValues(obj:any){
-
-  interface rez {
-    labels: any, 
-    values: any
-  }
-
-  let rez = { labels: null, values: null}
-
-  rez['labels'] = obj.payload.series.docs[0].period
-
-  rez['values']= obj.payload.series.docs[0].value
-
-
-  return rez
-
-
-}
-
-
-if(service.status=='loaded'){
-  // console.log(service.payload)
-  // console.log(service.payload.series.docs[0].value)
-
-
-    console.log(returnLabelsAndValues(service))
-
-  // console.log(dataOutput.payload)
-  // console.log(service)
-
-}
-
+  //   if (service.status == "loaded") {
+  //     setUnemploymentData(returnLabelsAndValues(service));
+  //     console.log(unemploymentData);
+  //   }
+  // }, []);
 
   return (
     <>
-    
-    <div>
-      <ControlPanel changeCountry={changeCountry} defaultCountry="Euro area" />
-    </div>
+      <div>
+        <ControlPanel
+          changeCountry={changeCountry}
+          defaultCountry="Euro area"
+        />
+      </div>
 
-    <div>
-{/* <ul>
-{service.status === 'loading' && <div>Loading...</div>}
-      {service.status === 'loaded' &&
-        service.payload.results.dataset.series.docs[0].value((row: any, ind: number) => (
-          <li key={ind}>{row}</li>
-        ))}
-      {service.status === 'error' && (
-        <div>Error, the backend moved to the dark side.</div>
-      )}
-
-</ul> */}
-   
-
-    </div>
+      <div></div>
     </>
-  
   );
 };
 
 export default DashContainer;
+
+// const [unemploymentURL, setUnemploymentURL] = useState<string>("");
+// const[unemployemnetSeries, setUnemploymentSeries] = useState ([])
+// const [vacanciesURL, setVacanciesURL] = useState<string>("");
+// const [netEarningsURL, setNetEarningsURL] = useState<string>("");
+// const [grossEarningsURL, setGrossEarningsURL] = useState<string>("");
+
+// useEffect(()=>{
+//   // export default selectedCountry
+//   // export default selectedCountry
+// }, [selectedCountry])
+// export default selectedCountry
+
+// let countryData = data[0];
+
+// function getKeyByValue(object: any, value: string) {
+//   return Object.keys(object).find((key) => object[key] === value);
+// }
+
+// function generateFetchURL_unemployemnt(selectedCountry: string) {
+//   let countryCode = getKeyByValue(countryData, selectedCountry);
+
+//   let fetchURL = `https://api.db.nomics.world/v22/series/Eurostat/une_rt_q/Q.SA.Y20-64.PC_ACT.T.${countryCode}?observations=True`;
+
+//   return fetchURL;
+// }
+
+// function generateFetchURL_vacancies(selectedCountry: string) {
+//   let countryCode = getKeyByValue(countryData, selectedCountry);
+
+//   let fetchURL = `https://api.db.nomics.world/v22/series/Eurostat/tps00172/Q.NSA.B-S.TOTAL.JOBRATE.${countryCode}?observations=True`;
+
+//   return fetchURL;
+// }
+
+// function generateFetchURL_net_earnings(selectedCountry: string) {
+//   let countryCode = getKeyByValue(countryData, selectedCountry);
+
+//   let fetchURL = `https://api.db.nomics.world/v22/series/Eurostat/earn_nt_net/A.EUR.GRS.A1_80.${countryCode}?observations=True`;
+
+//   return fetchURL;
+// }
+
+// function generateFetchURL_gross_earnings(selectedCountry: string) {
+//   let countryCode = getKeyByValue(countryData, selectedCountry);
+
+//   let fetchURL = `https://api.db.nomics.world/v22/series/Eurostat/earn_nt_net/A.PPS.GRS.A1_80.${countryCode}?observations=True`;
+
+//   return fetchURL;
+// }
+
+// useEffect(() => {
+//   setUnemploymentURL(generateFetchURL_unemployemnt(selectedCountry));
+// setVacanciesURL(generateFetchURL_vacancies(selectedCountry));
+// setNetEarningsURL(generateFetchURL_net_earnings(selectedCountry));
+// setGrossEarningsURL(generateFetchURL_gross_earnings(selectedCountry));
+// fetchUnemploymentData(unemploymentURL)
+// }, [selectedCountry]);
+
+// useEffect(()=>{
+
+//   function fetchUnemploymentData(unemploymentURL : string){
+//     fetch(unemploymentURL)
+//     .then(rez=>rez.json())
+//     // .then(data=>setUnemploymentSeries(data))
+//     .then(data=>console.log(data))
+//     // .then(()=> setUnemploymentSeries(unemployemnetSeries.dataset.series.docs[0].value))
+//     // .then(()=>console.log(unemployemnetSeries))
+//     .catch(err=>{console.log(err)})
+
+//       }
+
+//   fetchUnemploymentData(unemploymentURL)
+// }, [unemploymentURL])
+
+// let dataOutput = service
--- euro-labor-dash/src/services/useFetchEconService.tsx
+++ euro-labor-dash/src/services/useFetchEconService.tsx
@@ -1,64 +1,60 @@
 import React, { useState, useEffect } from "react";
 import data from "../dataSources/countryCodes.json";
-import {Service} from '../types/Service'
+import { Service } from "../types/Service";
 // import {EconResponse}  from 'C:/Users/Admin/Documents/js/react/euro-labor-dash/euro-labor-dash/src/types/EconResponse'
-
-
 
 let countryData = data[0];
 
 function getKeyByValue(object: any, value: string) {
-    return Object.keys(object).find((key) => object[key] === value);
-  }
+  return Object.keys(object).find((key) => object[key] === value);
+}
 
-  function generateFetchURL_unemployemnt(selectedCountry: string) {
+function generateFetchURL_unemployemnt(selectedCountry: string) {
+  let countryCode = getKeyByValue(countryData, selectedCountry);
+
+  // if (!countryCode) {
+  //   countryCode = "EA19";
+  // }
+
+  let fetchURL = `https://api.db.nomics.world/v22/series/Eurostat/une_rt_q/Q.SA.Y20-64.PC_ACT.T.${countryCode}?observations=True`;
+
+  return fetchURL;
+}
+
+export interface EconResponses {
+  // results: {dataset: any },
+  dataset: any;
+  provider: any;
+  series: any;
+  _meta: any;
+}
+
+const useFetchEconService = (selectedCountry: string) => {
+  const [result, setResult] = useState<Service<EconResponses>>({
+    status: "loading",
+  });
+
+  useEffect(() => {
+    const fetchURL = generateFetchURL_unemployemnt(selectedCountry);
+
     let countryCode = getKeyByValue(countryData, selectedCountry);
 
-    let fetchURL = `https://api.db.nomics.world/v22/series/Eurostat/une_rt_q/Q.SA.Y20-64.PC_ACT.T.${countryCode}?observations=True`;
+    if(countryCode){
+      fetch(fetchURL)
+      .then((res) => res.json())
+      .then((response) => setResult({ status: "loaded", payload: response }))
+      .catch((error) => setResult({ status: "error", error }));
 
-    return fetchURL;
-  }
-  
-  
-  interface insideFetchObject{
-      dataset :any
-  }
-  
-export interface EconResponses{
-    // results: {dataset: any },
-    dataset: any, 
-    provider: any, 
-    series: any, 
-    _meta: any
-
-    // results: EconResponse [ {datasinsideFetchObject}]
-    // results: EconResponse [{insideFetchObject}]
-    // results: EconResponse []
-    // EconResponse[]
-  }
-  
+    }
 
 
 
-  // move this to another folder? 
-  const useFetchEconService= (selectedCountry: string)=>{
-    const [result, setResult] = useState<Service<EconResponses>>({
-      status: 'loading'
-    })
+    // console.log(!countryCode);
+  // }, []);
+  }, [selectedCountry]);
 
-    useEffect(()=>{
+  return result;
+};
 
-        const fetchURL = generateFetchURL_unemployemnt(selectedCountry)
-
-        fetch(fetchURL)
-          .then(res =>res.json())
-          .then(response => setResult({ status: 'loaded', payload: response }))
-          .catch(error => setResult({ status: 'error', error }))
-//trigger on state update instaed of page load? 
-    }, [])
-
-    return result
-
-  }
-
-  export default useFetchEconService
+export default useFetchEconService;
+// export default fetchEconService
