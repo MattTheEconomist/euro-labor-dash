e7e7f4950ed2e26a0885320a0339c04213512cda
Matt Ashford <mattashford1@gmail.com>
2020-12-11 11:17:22 -0500

    earnings feature setup

--- euro-labor-dash/src/components/DashContainer.tsx
+++ euro-labor-dash/src/components/DashContainer.tsx
@@ -1,53 +1,39 @@
 import React, { useState, useEffect } from "react";
 import ControlPanel from "./ControlPanel";
 import data from "../dataSources/countryCodes.json";
-import { getKeyByValue, generateFetchURL_net} from '../services/URLgenerationFunctions'
+import {
+  getKeyByValue,
+  generateFetchURL_net,
+} from "../services/URLgenerationFunctions";
+import Earnings from './Earnings'
 
 
 const DashContainer: React.FC = () => {
   const [selectedCountry, setSelectedCountry] = useState<string>("Euro area");
   const [unemploymentData, setUnemploymentData] = useState({});
-  const [netEarningsData, setNetEarningsData] = useState({})
-
+  const [netEarningsData, setNetEarningsData] = useState({});
 
   let countryData = data[0];
-
-
-
-  // interface EconResponses {
-  //   dataset: any;
-  //   provider: any;
-  //   series: any;
-  //   _meta: any;
-  // }
 
   function fetchData() {
     // const fetchURL_unemployment: string = generateFetchURL_unemployemnt(selectedCountry);
 
-    const fetchURL_net :string = generateFetchURL_net(selectedCountry)
+    const fetchURL_net: string = generateFetchURL_net(selectedCountry);
 
-    let countryCode = getKeyByValue(countryData, selectedCountry);
+    // fetch(fetchURL_unemployment)
+    //   .then((res) => res.json())
+    //   .then((res) => {
+    //     setUnemploymentData(res);
+    //     console.log(unemploymentData);
+    //   })
+    //   .catch((error) => setUnemploymentData({ status: "error", error }));
 
-    if (countryCode) {
-      // fetch(fetchURL_unemployment)
-      //   .then((res) => res.json())
-      //   .then((res) => {
-      //     setUnemploymentData(res);
-      //     console.log(unemploymentData);
-      //   })
-      //   .catch((error) => setUnemploymentData({ status: "error", error }));
-
-        fetch(fetchURL_net)
-        .then((res)=>res.json())
-        .then((res)=> setNetEarningsData(res))
-        .catch((error) => setNetEarningsData({ status: "error", error }))
-        .then(()=>console.log(netEarningsData))
-
-          
-        }
-      
-    }
-  
+    fetch(fetchURL_net)
+      .then((res) => res.json())
+      .then((res) => setNetEarningsData(returnLabelsAndValues(res)))
+      .catch((error) => setNetEarningsData({ status: "error", error }))
+      .then(() => console.log(netEarningsData));
+  }
 
   useEffect(() => {
     fetchData();
@@ -57,37 +43,33 @@ const DashContainer: React.FC = () => {
     fetchData();
   }, [selectedCountry]);
 
-  // useEffect( fetchData, [])
-  // useEffect( fetchData, [selectedCountry])
-
-
   function changeCountry(newCountry: string): void {
     setSelectedCountry(newCountry);
   }
 
-  // function returnLabelsAndValues(obj: any): any | null {
-  //   interface rez {
-  //     labels?: any | undefined;
-  //     values?: any | undefined;
-  //     seriesName?: any | undefined;
-  //   }
+  function returnLabelsAndValues(obj: any): any | null {
+    interface rez {
+      labels?: any | undefined;
+      values?: any | undefined;
+      seriesName?: any | undefined;
+    }
 
-  //   let rez = {
-  //     labels: null,
-  //     values: null,
-  //     seriesName: null,
-  //   };
+    let rez = {
+      labels: null,
+      values: null,
+      seriesName: null,
+    };
 
-  //   if (Object.keys(obj.payload).length < 3) {
-  //     return rez;
-  //   }
+    if (Object.keys(obj).length < 3) {
+      return rez;
+    }
 
-  //   rez["labels"] = obj.payload.series.docs[0].period;
-  //   rez["values"] = obj.payload.series.docs[0].value;
-  //   rez["seriesName"] = obj.payload.series.docs[0].series_name;
+    rez["labels"] = obj.series.docs[0].period;
+    rez["values"] = obj.series.docs[0].value;
+    rez["seriesName"] = obj.series.docs[0].series_name;
 
-  //   return rez;
-  // }
+    return rez;
+  }
 
   return (
     <>
@@ -98,20 +80,13 @@ const DashContainer: React.FC = () => {
         />
       </div>
 
-      {/* <UnemploymentGraph /> */}
+      <div>{JSON.stringify(netEarningsData)}</div>
 
-
-<div>
-
- {JSON.stringify(netEarningsData)}
-</div>
-  {/* <div>{netEarningsData && netEarningsData.map(item=>{<li>{item}</li>})}</div> */}
+      <div>
+        <Earnings netEarningsData={netEarningsData} selectedCountry={selectedCountry}/>
+        </div>
     </>
   );
 };
 
 export default DashContainer;
-
-
-
-
--- euro-labor-dash/src/components/Earnings.tsx
+++ euro-labor-dash/src/components/Earnings.tsx
@@ -1,3 +1,18 @@
-import React from 'react'; 
+import React, { useEffect } from "react";
+import { scaleLinear, max, select } from "d3";
 
+interface EarningsProps {
+  netEarningsData: any;
+  selectedCountry: string;
+}
 
+const Earnings: React.FC<EarningsProps> = (
+  netEarningsData,
+  selectedCountry
+) => {
+
+    
+  return <h3>hi</h3>;
+};
+
+export default Earnings;
