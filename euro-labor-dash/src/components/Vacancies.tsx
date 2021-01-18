52c1268bcb7aafe876467635d3eab28213d7db99
Matt Ashford <mattashford1@gmail.com>
2021-01-18 09:49:40 -0500

    vacancy graph humble beginnings

--- euro-labor-dash/src/components/Vacancies.tsx
+++ euro-labor-dash/src/components/Vacancies.tsx
@@ -1,1 +1,22 @@
-import React from 'react'; 
+import React, { useEffect, useState } from "react";
+import { scaleLinear, max, min, mean, select } from "d3";
+import { create } from "domain";
+
+
+interface VacancyProps{
+
+    selectedCountry?: string;
+    netEarningsData: any;
+    isFetching: boolean;
+}
+
+
+
+
+const Vacancies: React.FC<VacancyProps>=({
+
+
+}) =>{
+
+    return null
+}
